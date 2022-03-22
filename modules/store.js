94f39ac83f45243c23f363297e78061dc01e8c34
lyamz <davidlyangenda@gmail.com>
2022-03-22 11:48:49 +0200

    Make changes to js and css

--- modules/store.js
+++ modules/store.js
@@ -1,29 +1,28 @@
-class Store {
-    getBooks = () => {
-      let books;
-      if (localStorage.getItem('booksStored') === null) {
-        books = [];
-      } else {
-        books = JSON.parse(window.localStorage.getItem('booksStored'));
-      }
-      return books;
+export default class Store {
+  getBooks = () => {
+    let books;
+    if (localStorage.getItem('booksStored') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(window.localStorage.getItem('booksStored'));
     }
+    return books;
+  }
 
-    addBook = (book) => {
-      const books = this.getBooks();
+  addBook = (book) => {
+    const books = this.getBooks();
 
-      books.push(book);
-      window.localStorage.setItem('booksStored', JSON.stringify(books));
-    }
+    books.push(book);
+    window.localStorage.setItem('booksStored', JSON.stringify(books));
+  }
 
-    removeBook = (button) => {
-      const books = this.getBooks();
-      const parentDiv = button.parentNode;
-      const myTitle = parentDiv.querySelector('.title').textContent;
-      const myAuthor = parentDiv.querySelector('.author').textContent;
-      const booksLeft = books.filter((book) => book.title !== myTitle && book.author !== myAuthor);
+  removeBook = (button) => {
+    const books = this.getBooks();
+    const parentDiv = button.parentNode;
+    const myTitle = parentDiv.querySelector('.title').textContent;
+    const myAuthor = parentDiv.querySelector('.author').textContent;
+    const booksLeft = books.filter((book) => book.title !== myTitle && book.author !== myAuthor);
 
-      window.localStorage.setItem('booksStored', JSON.stringify(booksLeft));
-    }
+    window.localStorage.setItem('booksStored', JSON.stringify(booksLeft));
+  }
 }
-export default Store;
